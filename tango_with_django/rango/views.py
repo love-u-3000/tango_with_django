2bd1815bd58b986fca09323b4b24e3b76f13f532
love-u-3000 <pavitra97agarwal@gmail.com>
2020-05-31 13:40:45 +0530

    added static files

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	response = "<html><body><h1>Rango says hello!</h1>"
-	response += "<a href = 'http://127.0.0.1:8000/rango/about' >About us</a></body></html>"
-	return HttpResponse(response)
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
+	return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
 	response = "<html><body><h1>Welcome to about us page</h1>"

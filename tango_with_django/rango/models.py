6f4fc09fec56235edead6806ac3f619c9e99f78e
love-u-3000 <pavitra97agarwal@gmail.com>
2020-06-01 12:56:34 +0530

    added models for rango

--- tango_with_django/rango/models.py
+++ tango_with_django/rango/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length = 128, unique = True)
+	visits = models.IntegerField(default = 0)
+
+	def __str__(self):
+		return self.name
+	
+class Page(models.Model):
+	views = models.IntegerField(default = 0)
+	title = models.CharField(max_length = 128)
+	category = models.ForeignKey('Category', on_delete = models.CASCADE)
+	url = models.URLField()
+
+	def __str__(self):
+		return self.title
+				

125e054d57c455c0dbc5764b0af58f4d32e5326c
love-u-3000 <pavitra97agarwal@gmail.com>
2020-06-02 13:37:17 +0530

    combined MVT and added slug

--- tango_with_django/rango/models.py
+++ tango_with_django/rango/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+from django.template.defaultfilters import slugify
+
 # Create your models here.
 
 class Category(models.Model):
 	name = models.CharField(max_length = 128, unique = True)
 	views = models.IntegerField(default = 0)
 	likes = models.IntegerField(default = 0)
+	slug = models.SlugField(blank = True, unique = True)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+
+	class Meta:
+		verbose_name_plural = 'categories'
+
 	def __str__(self):
 		return self.name
 	
@@ -15,6 +26,8 @@ class Page(models.Model):
 	category = models.ForeignKey('Category', on_delete = models.CASCADE)
 	url = models.URLField()
 
+	class Meta:
+		verbose_name_plural = 'All Pages'
+
 	def __str__(self):
 		return self.title
-				
--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Category, Page
+
 def index(request):
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
-	return render(request, 'rango/index.html', context = context_dict)
+	cat = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': cat}
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	context_dict = {'msg': 'hello dear friends'}

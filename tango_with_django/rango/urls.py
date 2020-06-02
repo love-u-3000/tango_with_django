509f50596e878de8025d7eae49f1e281b9e076ae
love-u-3000 <pavitra97agarwal@gmail.com>
2020-06-02 15:25:25 +0530

    added links for pages in categories

--- tango_with_django/rango/urls.py
+++ tango_with_django/rango/urls.py
@@ -1,7 +1,10 @@
 from rango import views
 from django.urls import path
 
+app_name = 'rango'
+
 urlpatterns = [
 	path('', views.index, name = 'index'),
 	path('about', views.about, name = 'about'),
+	path('category/<str:category_name_url>', views.show_category, name = 'category')
 ]
--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -4,6 +4,18 @@ from django.http import HttpResponse
 
 from .models import Category, Page
 
+def show_category(request, category_name_url):
+	context_dict = {}
+	try:
+		cat = Category.objects.get(slug = category_name_url)
+		pages = Page.objects.filter(category = cat)
+		context_dict['pages'] = pages
+		context_dict['category'] = cat
+	except Category.DoesNotExist:
+		context_dict['pages'] = None
+		context_dict['category'] = None
+	return render(request, 'rango/category.html', context_dict)
+
 def index(request):
 	cat = Category.objects.order_by('-likes')[:5]
 	context_dict = {'categories': cat}

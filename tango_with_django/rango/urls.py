d1f28bcdc596dcc065a3120956f058ffa2b12e04
love-u-3000 <pavitra97agarwal@gmail.com>
2020-06-05 17:08:30 +0530

    added support for register

--- tango_with_django/rango/urls.py
+++ tango_with_django/rango/urls.py
@@ -5,6 +5,7 @@ app_name = 'rango'
 
 urlpatterns = [
 	path('', views.index, name = 'index'),
+	path('register', views.register, name = 'register'),
 	path('about', views.about, name = 'about'),
 	path('category/<str:category_name_url>', views.show_category, name = 'category'),
 	path('add_category', views.add_category, name = 'add_category'),
